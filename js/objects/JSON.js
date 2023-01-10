ff6ed43e5d43d6c164191af84d341506b9002162
Sara Acuña <acunabenabidessaraelis@gmail.com>
2023-01-09 21:17:30 -0500

    objects in JS

---
+++ js/objects/JSON.js
@@ -0,0 +1,27 @@
+'user strict'
+// JSON ---->JavaScripts Object Notation
+window.addEventListener('load', function(){
+    var pelicula={ 
+        titulo:'Batman vrs Sirena',
+        year:2018,
+        country:"Colombia"
+    };
+    
+    var peliculas=[
+        {
+            titulo:"La vida",
+            year:2022,
+            pais:"Francia"
+        }, pelicula
+    ]
+
+    var cajaPeliculas=document.querySelector("#cajaPeliculas");
+
+    for (var indice in peliculas){
+        let p=document.createElement("p");
+        p.append(peliculas[indice].titulo+ " "+ peliculas[indice].year);
+        cajaPeliculas.append(p);
+    }
+
+})
+// Cambios actualizados
