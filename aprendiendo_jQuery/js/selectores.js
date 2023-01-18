b7b820962fe06faf325042750959dc59ed27fb25
Acuna21 <acunabenavidessaraelis@gmail.com>
2023-01-18 18:38:11 -0500

    selectores

---
+++ aprendiendo_jQuery/js/selectores.js
@@ -0,0 +1,65 @@
+// Libreria de JS
+// Reducir codigo de js puro, se utiliza mucho en proyectos monoliticos. 
+
+$(document).ready(function(){
+    console.log("Estamos listos");
+
+    // Seleccionar por ID
+    $("#1").css("background", "red")
+                    .css("color", "white");
+
+    $("#2").css("background", "green")
+                    .css("color", "white");
+
+    $("#3").css("background", "yellow")
+                    .css("color", "red");
+    
+    // Seleccionar por Clase
+    let claseJuntos=$(".juntos");
+
+    $(".solo").click(function(){
+        $(this).addClass("juntos");
+    })
+
+    // Sleccionar etiquetas
+    let parrafos=$("p");
+
+    parrafos.click(function(){
+        let that=$(this);
+        
+        if (!that.hasClass("grande")){
+            that.addClass("grande");
+        }
+        else{
+            that.removeClass("grande");
+        }
+    })
+
+
+    
+
+    // Selecctores de atributos
+    $("[title='Google']").css("color", "red");
+    $("[title='Facebook']").css("color", "red");
+    $("[title='Youtube']").css("color", "red");
+    
+    
+    
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+});
+
